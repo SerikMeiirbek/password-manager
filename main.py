e18c04b20bca2b7a71fd4275e5831fb2eae85545
serik <serik.meiirbek@gmail.com>
2024-03-17 10:27:46 -0400

    added password generator

--- main.py
+++ main.py
@@ -1,7 +1,70 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    print(f"Your password is: {password}")
+    pyperclip.copy(password)
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    if not validate_input_fields():
+        messagebox.showinfo(message="Please don't leave any fields empty!")
+        return
+
+    is_ok = messagebox.askokcancel(title=website_input,
+                                   message=f"These are the details are entered: \nEmail:{email_username_input.get()} "
+                                           f"\nPassword:{password_entry.get()} \nIs it ok to save?")
+
+    if is_ok:
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"\n{website_input.get()} | {email_username_input.get()} | {password_entry.get()}")
+        clear_input_fields()
+
+
+# -------------------------- Clear input fields -------------------------- #
+def clear_input_fields():
+    website_input.delete(0, END)
+    email_username_input.delete(0, END)
+    password_entry.delete(0, END)
+
+
+# -------------------------- Check input fields -------------------------- #
+def validate_input_fields():
+    return website_input.get() != "" and email_username_input.get() != "" and password_entry.get() != ""
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -36,12 +99,11 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-password_generator_btn = Button(text="Generate Password")
+password_generator_btn = Button(text="Generate Password", command=generate_password)
 password_generator_btn.grid(row=3, column=2)
 
 # Add
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save_password)
 add_btn.grid(row=4, column=1, columnspan=2)
-
 
 window.mainloop()
